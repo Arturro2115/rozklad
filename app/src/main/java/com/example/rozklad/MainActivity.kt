12268e2809b07720e2f0773e59aab467ef8cab63
turcio <arturgunter2006@gmail.com>
2022-12-17 20:00:26 +0100

    2

--- app/src/main/java/com/example/rozklad/MainActivity.kt
+++ app/src/main/java/com/example/rozklad/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.rozklad
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.oblicz) .setOnClickListener{
+            var x = findViewById<EditText>(R.id.liczba).text.toString().toInt()
+            var y = 2
+            val wynik = mutableListOf<String>()
+
+            do {
+                do {
+                    val x2 = x.toString()
+                    wynik += x2
+                    x /= y
+                }
+                    while (x % y == 0)
+                    x +=1
+            }
+                while (x>1)
+                findViewById<TextView>(R.id.wynik).text = wynik.toString()
+        }
     }
 }
